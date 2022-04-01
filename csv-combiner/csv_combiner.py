48d94411e26e8175e8a6836853543e38a2ae7646
Jerry <jerryputh2000@gmail.com>
2022-04-01 16:08:27 -0400

    Added comments

--- csv-combiner/csv_combiner.py
+++ csv-combiner/csv_combiner.py
@@ -3,8 +3,10 @@
 import sys
 import os
 
+
 class CSV_Combiner:
     def __init__(self, files):
+        # Checks that files are all .csv files
         for arg in files:
             if not arg.endswith(".csv"):
                 print("One of the files does not end with .csv. Try again.")
@@ -12,28 +14,37 @@ class CSV_Combiner:
         self.files = files
 
     def combine(self, files):
+        # Goes through each file
         for fileNum, filename in enumerate(files):
             basename = os.path.basename(filename)
             try:
+                # Checks for header row
                 first_row = True
+                # Tries to open file. Exits otherwise.
                 with open(filename) as f:
+                    # Combine each line
                     for line in f:
                         line = line.strip("\n")
+                        # If it is not the first file and the line is the header row. Don't want to output header so
+                        # continue.
                         if first_row and fileNum > 0:
                             first_row = False
                             continue
+                        # If it is an empty line, continue
                         elif line == "":
                             continue
+                        # If it is the first file and the line is the header row, output and add column name
                         elif first_row:
                             print(line + "," + "\"filename\"")
                             first_row = False
+                        # Otherwise combine line with file name
                         else:
                             print(line + "," + "\"" + basename + "\"")
+            # Handles error when file cannot be opened.
             except FileNotFoundError:
                 print(filename + " cannot be found/opened. All the files up to the file have been combined. Try again.")
                 return 1
         return 0
-
 
 
 if __name__ == "__main__":
@@ -43,5 +54,7 @@ if __name__ == "__main__":
     if len(args) < 1:
         print("Try again. Missing csv files to combine. Usage: csv_combiner.py [files].")
         quit()
+    # Initializes CSV_combiner class
     combiner = CSV_Combiner(args)
+    # Calls combine method
     combiner.combine(args)
--- csv-combiner/csv_combiner_tester.py
+++ csv-combiner/csv_combiner_tester.py
@@ -15,13 +15,11 @@ class TestCSV_Combiner(unittest.TestCase):
             CSV_Combiner(["csv_combiner.py"]).combine(["csv_combiner.py"])
         self.assertEqual(cm.exception.code, 1)
 
-
     # Tests combine method with non existent csv file
     def test_nonExistantCSVFile(self):
         self.assertTrue(CSV_Combiner(["nonexistant.csv"]).combine(["nonexistant.csv"]), 1)
 
-
+    # Tests that combine works
     def test_combine(self):
-        self.assertTrue(subprocess.run(["python", "csv_combiner.py", "./fixtures/clothing.csv", "./fixtures/accessories.csv"]), 0)
-
-
+        self.assertTrue(
+            subprocess.run(["python", "csv_combiner.py", "./fixtures/clothing.csv", "./fixtures/accessories.csv"]), 0)
